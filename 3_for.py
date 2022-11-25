fe13ff0ddf17e1d6e2c7de6fee9836c87641d431
Artem Voskresensky <metravs@gmail.com>
2022-11-25 13:34:30 +0300

    Задача 3

--- 3_for.py
+++ 3_for.py
@@ -17,11 +17,29 @@
 """
 
 def main():
+    
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    sales =  [
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+  ]
+    all_sales, avg_sales = 0, 0
+    print('Общее количество продаж:')
+    for el in sales:
+        unit_sale = sum(el["items_sold"])
+        all_sales += unit_sale
+        print(f'- {el["product"]}: {unit_sale}')
+    print('Среднее количество продаж:')
+    for el in sales:
+        avg_unit_sale = round(sum(el["items_sold"])/len(el["items_sold"]), 2)
+        avg_sales += avg_unit_sale
+        print(f'- {el["product"]}: {avg_unit_sale}')
+    print('Суммарное количество продаж всех товаров: ', all_sales)
+    print('Среднее количество продаж всех товаров: ', avg_sales)
     
 if __name__ == "__main__":
     main()
