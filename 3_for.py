6eb244863e00967493f92923704ed34ea61373ac
Artem Voskresensky <metravs@gmail.com>
2022-11-25 14:20:58 +0300

    Исправления к задаче 3

--- 3_for.py
+++ 3_for.py
@@ -28,16 +28,21 @@ def main():
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
     all_sales, avg_sales = 0, 0
+    total_sales_count = 0
     print('Общее количество продаж:')
     for el in sales:
         unit_sale = sum(el["items_sold"])
         all_sales += unit_sale
-        print(f'- {el["product"]}: {unit_sale}')
+        print('- {product}: {unit_sale}'.format(product = el["product"], unit_sale = unit_sale))
     print('Среднее количество продаж:')
     for el in sales:
-        avg_unit_sale = round(sum(el["items_sold"])/len(el["items_sold"]), 2)
-        avg_sales += avg_unit_sale
-        print(f'- {el["product"]}: {avg_unit_sale}')
+        total_sales = sum(el["items_sold"])
+        sales_count = len(el["items_sold"])
+        total_sales_count += sales_count
+        avg_unit_sale = round(total_sales/sales_count, 2)
+        print('- {product}: {avg_unit_sale}'.format(product = el["product"], avg_unit_sale = avg_unit_sale))
+
+    avg_sales = all_sales / total_sales_count         
     print('Суммарное количество продаж всех товаров: ', all_sales)
     print('Среднее количество продаж всех товаров: ', avg_sales)
     
